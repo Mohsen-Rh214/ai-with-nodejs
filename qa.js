b518c2ca465a822c6c80b04f8114998facc8c79b
mohsen-rh214 <mohsen.rh0214@gmail.com>
2024-01-17 18:04:55 +0330

    added pdf doc and search function // yt search is not working(?)

--- qa.js
+++ qa.js
@@ -6,13 +6,14 @@ import { CharacterTextSplitter } from 'langchain/text_splitter'
 import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
 import { openai } from './openai.js'
 
+/// to run this code: node qa "what is my question?"
 const question = process.argv[2] || 'hi' // like this: node(argv[0]) run(argv[1]) "something else"(argv[2])
 const video = `https://youtu.be/zR_iuq2evXo?si=cG8rODgRgXOx9_Cn`
 
 const createStore = (docs) =>
   MemoryVectorStore.fromDocuments(docs, new OpenAIEmbeddings())
 
-const docsFromYTVideo = (video) => {
+const docsFromYTVideo = async (video) => {
   const loader = YoutubeLoader.createFromUrl(video, {
     language: 'en',
     addVideoInfo: true,
@@ -26,3 +27,54 @@ const docsFromYTVideo = (video) => {
   )
 }
 
+const docsFromPDF = () => {
+  const loader = new PDFLoader('xbox.pdf')
+  return loader.loadAndSplit(
+    new CharacterTextSplitter({
+      separator: '. ',
+      chunkSize: 2500,
+      chunkOverlap: 200,
+    })
+  )
+}
+
+// creating store
+const loadStore = async () => {
+  const videoDocs = await docsFromYTVideo(video)
+  const pdfDocs = await docsFromPDF()
+
+  console.log('video: ', videoDocs[0])
+
+  return createStore([...videoDocs, ...pdfDocs])
+}
+
+const query = async () => {
+  const store = await loadStore()
+  const results = await store.similaritySearch(question, 1)
+
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo-16k-0613',
+    temperature: 0,
+    messages: [
+      {
+        role: 'assistant',
+        content:
+          'You are a helpful AI assistant. Answser questions to your best ability.',
+      },
+      {
+        role: 'user',
+        content: `Answer the following question using the provided context. If you cannot answer the question with the context, don't lie and don't make up stuff. Just say you need more context.
+        Question: ${question}
+  
+        Context: ${results.map((r) => r.pageContent).join('\n')}`,
+      },
+    ],
+  })
+  console.log(
+    `Answer: ${response.choices[0].message.content}\n\nSources: ${results
+      .map((r) => r.metadata.source)
+      .join(', ')}`
+  )
+}
+
+query()
